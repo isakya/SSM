5017449b193e34fe2079b9d941cc305cca935e43
konglong <1016361268@qq.com>
2023-01-20 01:02:03 +0800

    使用全局配置处理字段名和属性名不一致的情况

--- mybatis_resultMap/src/main/java/com/izumi/mybatis/mapper/EmpMapper.java
+++ mybatis_resultMap/src/main/java/com/izumi/mybatis/mapper/EmpMapper.java
@@ -1,4 +1,10 @@
 package com.izumi.mybatis.mapper;
 
+import com.izumi.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
 public interface EmpMapper {
+
+    // 根据id查询员工信息
+    Emp getEmpByEmpId(@Param("empId") Integer empId);
 }
---
+++ mybatis_resultMap/src/test/java/ResultMapTest.java
@@ -0,0 +1,15 @@
+import com.izumi.mybatis.mapper.EmpMapper;
+import com.izumi.mybatis.pojo.Emp;
+import com.izumi.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class ResultMapTest {
+    @Test
+    public void testGetEmpByEmpId() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpByEmpId(1);
+        System.out.println(emp);
+    }
+}
