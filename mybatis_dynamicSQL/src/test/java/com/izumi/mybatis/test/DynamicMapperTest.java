39d6a8242068e936e9b3c8eea7c16ca7e1749ab9
konglong <1016361268@qq.com>
2023-01-20 16:34:20 +0800

    mybatis的动态sql

--- mybatis_dynamicSQL/src/test/java/com/izumi/mybatis/test/DynamicMapperTest.java
+++ mybatis_dynamicSQL/src/test/java/com/izumi/mybatis/test/DynamicMapperTest.java
@@ -6,6 +6,7 @@ import com.izumi.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class DynamicMapperTest {
@@ -17,5 +18,33 @@ public class DynamicMapperTest {
         Emp emp = new Emp(null, "张三", 20, "男");
         List<Emp> list = mapper.getEmpByCondition(emp);
         list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetEmpByChoose() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        Emp emp = new Emp(null, "张三", 20, "男");
+        List<Emp> list = mapper.getEmpByChoose(emp);
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testInsertMoreEmp() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        Emp emp1 = new Emp(null, "张三1", 20, "男");
+        Emp emp2 = new Emp(null, "张三2", 20, "男");
+        Emp emp3 = new Emp(null, "张三3", 20, "男");
+        List<Emp> list = Arrays.asList(emp1, emp2, emp3);
+        mapper.insertMoreEmp(list);
+    }
+
+    @Test
+    public void testDeleteMoreEmp() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        Integer[] empIds = new Integer[]{6,7};
+        mapper.deleteMoreEmp(empIds);
     }
 }
