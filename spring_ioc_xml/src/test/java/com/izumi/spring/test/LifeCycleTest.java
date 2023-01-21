f49edf4246336bad94487fe94b13b8964a290b74
konglong <1016361268@qq.com>
2023-01-22 01:25:27 +0800

    bean的后置处理器

--- spring_ioc_xml/src/test/java/com/izumi/spring/test/LifeCycleTest.java
+++ spring_ioc_xml/src/test/java/com/izumi/spring/test/LifeCycleTest.java
@@ -9,8 +9,19 @@ public class LifeCycleTest {
      *      生命周期步骤：
      *          1. 实例化
      *          2. 依赖注入
-     *          3. 初始化，需要通过bean的init-method属性指定初始化的方法
-     *          4. IOC容器关闭时销毁，需要通过bean的destroy-method属性指定销毁的方法
+     *          3. 后置处理器的postProcessBeforeInitialization
+     *          4. 初始化，需要通过bean的init-method属性指定初始化的方法
+     *          5. 后置处理器的postProcessAfterInitialization
+     *          6. IOC容器关闭时销毁，需要通过bean的destroy-method属性指定销毁的方法
+     *
+     *          注意：
+     *              若bean的作用域为单例时，生命周期的前三个步骤会在获取IOC容器时执行
+     *              若bean的作用域为多例时，生命周期的前三个步骤会在获取bean时执行
+     *
+     *
+     *           bean的后置处理器-->会在生命周期的初始化前后添加额外的操作，需要实现BeanPostProcessor接口，
+     *          且配置到IOC容器中，需要注意的是，bean后置处理器不是单独针对某一个bean生效，而是针对IOC容
+     *          器中所有bean都会执行
      */
     @Test
     public void test() {
