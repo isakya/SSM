3f834aa4581fa66552a2e1b9f13a20daf4ba6c73
konglong <1016361268@qq.com>
2023-01-24 22:16:45 +0800

    通过注解设置的id来获取bean

--- spring_ioc_annotation/src/main/java/com/izumi/spring/controller/UserController.java
+++ spring_ioc_annotation/src/main/java/com/izumi/spring/controller/UserController.java
@@ -2,6 +2,6 @@ package com.izumi.spring.controller;
 
 import org.springframework.stereotype.Controller;
 
-@Controller
+@Controller("controller")
 public class UserController {
 }
--- spring_ioc_annotation/src/test/java/com/izumi/spring/test/IOCByAnnotationTest.java
+++ spring_ioc_annotation/src/test/java/com/izumi/spring/test/IOCByAnnotationTest.java
@@ -12,15 +12,19 @@ public class IOCByAnnotationTest {
      *  @Controller：将类标识为控制层组件
      *  @Service：将类标识为业务层组件
      *  @Repository：将类标识为持久层组件
+     *
+     *
+     *  通过注解+扫描所配置的id，默认值为类的小驼峰，即类名首字母为小写的结果
+     *  可以通过标识组件的注解的value属性值设置bean的自定义id
      */
     @Test
     public void test() {
         ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("spring-ioc-annotation.xml");
-        UserController userController = ioc.getBean(UserController.class);
+        UserController userController = ioc.getBean("controller",UserController.class);
         System.out.println(userController);
-        UserService userService = ioc.getBean(UserService.class);
+        UserService userService = ioc.getBean("userServiceImpl" ,UserService.class);
         System.out.println(userService);
-        UserDao userDao = ioc.getBean(UserDao.class);
+        UserDao userDao = ioc.getBean("userDaoImpl",UserDao.class);
         System.out.println(userDao);
     }
 }
