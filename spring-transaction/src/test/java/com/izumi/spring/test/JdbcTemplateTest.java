35c7188f5e010f4055946523f026a0ed2e9b2f6c
konglong <1016361268@qq.com>
2023-01-27 20:40:08 +0800

    JdbcTemplate实现查询功能

--- spring-transaction/src/test/java/com/izumi/spring/test/JdbcTemplateTest.java
+++ spring-transaction/src/test/java/com/izumi/spring/test/JdbcTemplateTest.java
@@ -1,11 +1,15 @@
 package com.izumi.spring.test;
 
+import com.izumi.spring.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 // 指定当前测试类在Spring的测试环境中执行，此时就可以通过注入的方式直接获取IOC容器中的bean
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -20,4 +24,26 @@ public class JdbcTemplateTest {
         String sql = "insert into t_user values(null, ?,?,?,?,?)";
         jdbcTemplate.update(sql, "root", "123", 23, "女", "123@qq.com");
     }
+
+    @Test
+    public void testGetUserById(){
+        String sql = "select * from t_user where id = ?";
+        User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), 1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testGetAllUser() {
+        String sql = "select * from t_user";
+        List<User> list = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetCount() {
+        String sql = "select count(*) from t_user";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+        System.out.println(count);
+    }
+
 }
