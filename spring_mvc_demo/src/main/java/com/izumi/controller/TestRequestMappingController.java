dc17e25f9a89f001f67e505cd82ff4fbf39e95e9
konglong <1016361268@qq.com>
2023-01-28 18:02:18 +0800

    @RequestMapping注解的method属性

--- spring_mvc_demo/src/main/java/com/izumi/controller/TestRequestMappingController.java
+++ spring_mvc_demo/src/main/java/com/izumi/controller/TestRequestMappingController.java
@@ -2,6 +2,7 @@ package com.izumi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /***
  * 1、@RequestMapping注解标识的位置
@@ -11,11 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 作用：通过请求的请求路径匹配请求
  * value属性是数组类型，即当前浏览器所发送请求的请求路径匹配value属性中的任何一个值
  * 则当前请求就会被注解所标识的方法进行处理
+ *
+ * 3、@RequestMapping注解的method属性
+ * 作用：通过请求的请求方式匹配请求
+ *      method属性是RequestMethod类型的数组，即当前浏览器所发送的请求方式匹配method属性中的任何一种请求方式，
+ *      则当前请求就会被注解所标识的方法进行处理
  */
 @Controller
 // @RequestMapping("/test")
 public class TestRequestMappingController {
-    @RequestMapping({"/hello", "/abc"})
+    @RequestMapping(
+            value = {"/hello", "/abc"},
+            method = {RequestMethod.POST, RequestMethod.GET}
+    )
     public String hello() {
         return "success";
     }
