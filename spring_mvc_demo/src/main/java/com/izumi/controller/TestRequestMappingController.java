a183d45c276a11d83fab1847e858c1892a58dc13
konglong <1016361268@qq.com>
2023-01-28 16:45:38 +0800

    @RequestMapping注解的value属性

--- spring_mvc_demo/src/main/java/com/izumi/controller/TestRequestMappingController.java
+++ spring_mvc_demo/src/main/java/com/izumi/controller/TestRequestMappingController.java
@@ -3,10 +3,19 @@ package com.izumi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/***
+ * 1、@RequestMapping注解标识的位置
+ * @RequestMapping标识一个类：设置映射请求的请求路径的初始信息
+ * @RequestMapping标识一个方法：设置映射请求路径的具体信息
+ * 2、@RequestMapping注解value属性
+ * 作用：通过请求的请求路径匹配请求
+ * value属性是数组类型，即当前浏览器所发送请求的请求路径匹配value属性中的任何一个值
+ * 则当前请求就会被注解所标识的方法进行处理
+ */
 @Controller
-@RequestMapping("/test")
+// @RequestMapping("/test")
 public class TestRequestMappingController {
-    @RequestMapping("/hello")
+    @RequestMapping({"/hello", "/abc"})
     public String hello() {
         return "success";
     }
