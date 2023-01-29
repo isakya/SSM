f4a6c05d9ba82e52a32e44b788ee0b9f884b134a
konglong <1016361268@qq.com>
2023-01-30 01:38:55 +0800

    向会话域和应用域共享数据

--- spring_mvc_demo/src/main/java/com/izumi/controller/TestScopeController.java
+++ spring_mvc_demo/src/main/java/com/izumi/controller/TestScopeController.java
@@ -1,14 +1,26 @@
 package com.izumi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 /**
  *      向域对象共享数据：
  *          1、通过ModelAndView向请求域共享数据
  *              使用ModelAndView时，可以使用其Model功能向请求域共享数据
  *              使用View功能设置逻辑视图，但是控制器方法一定要将ModelAndView作为方法的返回值
+ *
+ *          2、使用Model向请区域共享数据
+ *          3、使用ModelMap向请区域共享数据
+ *          4、使用map向请区域共享数据
+ *
+ *          5、Model和ModelMap和map的关系
+ *              其实在底层中，这些类型的形参最终都是通过BindingAwareModelMap创建
+ *
  *
  */
 
@@ -28,5 +40,23 @@ public class TestScopeController {
         // 设置逻辑视图实现页面跳转
         mav.setViewName("success");
         return mav;
+    }
+
+    @RequestMapping("/test/model")
+    public String testModel(Model model) {
+        model.addAttribute("testRequestScope", "hello Model");
+        return "success";
+    }
+
+    @RequestMapping("/test/modelMap")
+    public String testModelMap(ModelMap modelMap) {
+        modelMap.addAttribute("testRequestScope", "hello ModelMap");
+        return "success";
+    }
+
+    @RequestMapping("/test/map")
+    public String testMap(Map<String, Object> map) {
+        map.put("testRequestScope", "hello map");
+        return "success";
     }
 }
