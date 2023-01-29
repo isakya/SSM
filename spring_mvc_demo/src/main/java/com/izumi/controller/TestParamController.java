0e339fd14084b545a78f010e58ae0268b0d4d856
konglong <1016361268@qq.com>
2023-01-30 00:16:23 +0800

    通过pojo获取请求参数

--- spring_mvc_demo/src/main/java/com/izumi/controller/TestParamController.java
+++ spring_mvc_demo/src/main/java/com/izumi/controller/TestParamController.java
@@ -1,5 +1,6 @@
 package com.izumi.controller;
 
+import com.izumi.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +23,10 @@ import javax.servlet.http.HttpSession;
  *
  * 4、@RequestHeader: 将请求头信息和控制器方法的形参绑定
  * 5、@RequestValue: 将cookie数据和控制器方法的形参绑定
+ *
+ * 6、通过控制器方法的实体类类型的形参获取请求参数
+ *      需要在控制器方法的形参位置设置实体类类型的形参，要保证实体类中的属性的属性名和请求参数的名字一致，
+ *      可以通过实体类类型的形参获取请求参数
  */
 
 @Controller
@@ -46,6 +51,12 @@ public class TestParamController {
         System.out.println("jsessionId:" + jsessionId);
         System.out.println(username + " " + password);
         System.out.println(referer);
+        return "success";
+    }
+
+    @RequestMapping("/param/pojo")
+    public String getParamByPojo(User user) {
+        System.out.println(user);
         return "success";
     }
 }
