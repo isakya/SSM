48fce4cb1ad1a104a9f2e77268ce98769be5d7ff
konglong <1016361268@qq.com>
2023-01-30 02:13:30 +0800

    SpringMVC的视图之ThymeleafView

---
+++ spring_mvc_demo/src/main/java/com/izumi/controller/TestViewController.java
@@ -0,0 +1,13 @@
+package com.izumi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestViewController{
+    @RequestMapping("/test/view/thymeleaf")
+    public String testThymeleafView() {
+
+        return "success";
+    }
+}
