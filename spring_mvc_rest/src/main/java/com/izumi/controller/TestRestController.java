c82bcf728cf7feec5ea4d5733620a03b27abcc4e
konglong <1016361268@qq.com>
2023-01-30 03:31:19 +0800

    RESTful之测试查询功能

---
+++ spring_mvc_rest/src/main/java/com/izumi/controller/TestRestController.java
@@ -0,0 +1,30 @@
+package com.izumi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *      查询所有的用户信息 --> /user --> get
+ *      根据id查询用户信息 --> /user/1 --> get
+ *      添加用户信息 --> /user --> post
+ *      修改用户信息 --> /user --> put
+ *      删除用户信息 --> /user/1 --> delete
+ */
+
+@Controller
+public class TestRestController {
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String getAllUser() {
+        System.out.println("查询所有的用户信息--> /user --> get");
+        return "success";
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public String getUserById(@PathVariable("id") Integer id) {
+        System.out.println("根据id查询用户信息--> /user/ "+ id +" --> get");
+        return "success";
+    }
+
+}
