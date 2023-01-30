868861b15dc8397c6873bd231d5daf8254b1a3ac
konglong <1016361268@qq.com>
2023-01-30 20:17:44 +0800

    测试SpringMVC处理ajax请求

--- spring_mvc_ajax/src/main/java/com/izumi/controller/TestAjaxController.java
+++ spring_mvc_ajax/src/main/java/com/izumi/controller/TestAjaxController.java
@@ -1,7 +1,16 @@
 package com.izumi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class TestAjaxController {
+    @RequestMapping("/test/ajax")
+    public void testAjax(Integer id, HttpServletResponse response) throws IOException {
+        System.out.println("id:" + id);
+        response.getWriter().write("hello, axios");
+    }
 }
